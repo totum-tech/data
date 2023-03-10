c0cbd583589d7ad99c3f8aeb64eb00f386ecd9c5
Marcus <marcus@totum.io>
2023-03-10 15:01:26 -0800

    MST Stream works

---
+++ src/event-store-mst/stream.ts
@@ -0,0 +1,92 @@
+import {IObservableArray, values} from 'mobx';
+import {Instance, types} from 'mobx-state-tree';
+import {parseISO} from 'date-fns';
+
+const GenericData = types.model('GenericData', {
+  message: types.string,
+});
+
+export const RecordedEvent = types.model('RecordedEvent', {
+  id: types.identifier,
+  streamId: types.string,
+  streamEventRevision: types.number,
+  globalEventPosition: types.number,
+  type: types.string,
+  createdAt: types.Date,
+  data: GenericData,
+}).preProcessSnapshot(snapshot => {
+  if (typeof snapshot.createdAt === 'string') {
+    return {
+    ...snapshot,
+      // @ts-ignore
+      createdAt: parseISO(snapshot.createdAt)
+    }
+  } else {
+    return snapshot
+  }
+
+});
+
+type RecordedEventType = Instance<typeof RecordedEvent>;
+
+export function createEventStream(params) {
+  const EventStream = types.model('EventStream', {
+    events: types.optional(types.map(RecordedEvent), {})
+  }).actions(stream => {
+    async function initialize() {
+      const loadedEvents = await params.storage.load();
+      // @ts-ignore
+      loadedEvents.forEach(event => stream.setEvent(event.id, event));
+    }
+
+    function setEvent(id, event) {
+      stream.events.set(event.id, event);
+    }
+
+    function listenForEvents() {
+      params.storage.subscribe(event => {
+        // @ts-ignore
+        const eventExistsLocally = stream.isEventRecorded(event);
+        if (eventExistsLocally) {
+          console.info('Event already recorded locally.');
+          return;
+        } else {
+          // @ts-ignore
+          stream.setEvent(event.id, event);
+        }
+      })
+    }
+
+    function appendEvent(recordedEvent: any) {
+      // @ts-ignore
+      stream.setEvent(recordedEvent.id, recordedEvent);
+      return params.storage.save(recordedEvent);
+    }
+
+    return {
+      initialize,
+      setEvent,
+      appendEvent,
+      listenForEvents,
+    }
+  }).views(stream => ({
+    isEventRecorded: event => {
+      return !!stream.events.get(event.id);
+    },
+    // get size() {
+    //   return values(stream.events).length;
+    // },
+    getEvents() {
+      return values(stream.events);
+    },
+    // eventsForStream(streamId: string) {
+    //   function filterFn(event: RecordedEventType): boolean {
+    //     return event.streamId === streamId;
+    //   }
+    //   // @ts-ignore
+    //   return stream.events.filter(filterFn);
+    // }
+  }));
+
+  return EventStream.create({ events: {} });
+}
