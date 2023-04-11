859f54429e5e7d058376a5e4aefce0d1ebeed115
Marcus <marcus@totum.io>
2023-04-11 11:52:13 -0700

    Importing from command

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as FirebaseEventStorage } from './storage/fb-event-storage';
 export { default as MemoryEventStorage } from './storage/memory-event-storage';
 export { default as SupabaseEventStorage } from './storage/supabase-event-storage';
 export { EventStream, EventStore } from './event-store';
+export * from './command';
